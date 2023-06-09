3dc059e1e0d7f897634718d0fb3bc04283bd4c20
Houroux <arthurtrovato@gmail.com>
2023-06-09 10:46:41 +0200

    Feat : Banner integration

--- kasa/src/components/HomeBanner/index.jsx
+++ kasa/src/components/HomeBanner/index.jsx
@@ -1,7 +1,6 @@
-import React from 'react'
+import React from "react";
+import "../../style/homeBanner.scss";
 
 export default function HomeBanner() {
-  return (
-    <div>HomeBanner</div>
-  )
+	return <div className="homeBanner">Chez vous, partout et ailleurs</div>;
 }
